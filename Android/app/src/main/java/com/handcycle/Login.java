cce5a003d018c8b2de44b637d1fa5bcd558afb37
Jerry Chong <15714095+jerrychong25@users.noreply.github.com>
2019-12-30 00:19:29 +0800

    Migrated all old dependencies to latest AndroidX version

--- Android/app/src/main/java/com/handcycle/Login.java
+++ Android/app/src/main/java/com/handcycle/Login.java
@@ -3,10 +3,13 @@ package com.handcycle;
 // Import Android API
 import android.content.Intent;
 import android.os.Build;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
+//import android.support.v4.content.ContextCompat;
+import androidx.core.content.ContextCompat;
+//import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+//import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
